7a5b5e68f48dcb819289ebf891b0d2c0c734928c
Washington Leandro <sntslleandro@gmail.com>
2022-11-21 11:17:48 -0300

    Finalização do programa

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,10 +8,11 @@ import entities.Product;
 public class Program {
 
 	public static void main(String[] args) {
-		// Resolução por Nelio Alves (resolvendo conforme ele resolve no curso para eu entender o codigo)
+		// Resolução por Nelio Alves (resolvendo conforme ele resolve no curso para eu
+		// entender o codigo)
 		Locale.setDefault(Locale.US);
 		Scanner scan = new Scanner(System.in);
-		
+
 		Product product = new Product();
 		System.out.println("Enter product data: ");
 		System.out.print("Name: ");
@@ -20,11 +21,28 @@ public class Program {
 		product.price = scan.nextDouble();
 		System.out.print("Quantity in stock: ");
 		product.quantity = scan.nextInt();
-		
-		//System.out.println(product.name + ", " + product.price + ", " + product.quantity);
-		
-		System.out.println(product);
-		
+
+		// System.out.println(product.name + ", " + product.price + ", " +
+		// product.quantity);
+		System.out.println();
+		System.out.println("Product data: " + product);
+
+		System.out.println();
+		System.out.print("Enter the number of products to be added in stock: ");
+		int quantity = scan.nextInt();
+		product.addProducts(quantity);
+
+		System.out.println();
+		System.out.println("Updated data: " + product);
+
+		System.out.println();
+		System.out.print("Enter the number of products to be removed in stock: ");
+		quantity = scan.nextInt();
+		product.removeProducts(quantity);
+
+		System.out.println();
+		System.out.println("Updated data: " + product);
+
 		scan.close();
 
 	}
